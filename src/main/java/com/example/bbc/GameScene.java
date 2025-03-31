ba31bc1e21c72d9d312bb8b29f92d497374b91fa
DymNomZ <dymierborgonia@gmail.com>
2025-03-31 21:51:28 +0800

    Transfering main game logic to GameScene class
    Changes:
    - Modified GameScene.java
    - Modified IOGame.java

--- src/main/java/com/example/bbc/GameScene.java
+++ src/main/java/com/example/bbc/GameScene.java
@@ -1,17 +1,27 @@
 package com.example.bbc;
 
 import classes.Player;
+import entities.Entity;
+import entities.ProjectileEntity;
+import entities.TankEntity;
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+import java.util.*;
+import java.util.List;
 
-public class GameScene extends Scene implements Runnable {
+public class GameScene extends Scene {
 
     public static int FPS = 60; // delta loop purposes
     public static final int SCALE = 2;
@@ -28,111 +38,93 @@ public class GameScene extends Scene implements Runnable {
     public Thread main_thread;
     Player d1;
 
-    static FXMLLoader fxmlLoader = new FXMLLoader(IOGame.class.getResource("game-panel.fxml"));
-
-    static Parent root;
-
-    Canvas canvas = new Canvas(300, 200);
-    GraphicsContext gc = canvas.getGraphicsContext2D();
-
-//    static {
-//        try {
-//            root = fxmlLoader.load(canvas);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    static Group root = new Group();
+    private Entity mainPlayer;
+    private final Set<KeyCode> activeKeys = new HashSet<>();
+    private double mouse_x, mouse_y;
+    public static List<Entity> entityList;
 
     public GameScene() {
         super(root, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-        key_input = new KeyHandler();
-        mouse_handler = new MouseHandler();
-        map = new MapConstructor("assets/maps/square_test.zip");
+        javafx.scene.image.Image backgroundImage = new Image("file:src/main/java/assets/background.png");
+        ImageView backgroundView = new ImageView(backgroundImage);
+        backgroundView.fitHeightProperty().bind(IOGame.MAIN_STAGE.heightProperty());
+        backgroundView.fitWidthProperty().bind(IOGame.MAIN_STAGE.widthProperty());
 
-        max_map_row = map.getMap_height();
-        max_map_col = map.getMap_length();
-        //System.out.println(max_map_col + " " + max_map_row);
+        root.getChildren().add(backgroundView);
 
-        d1 = new Player("Amogus", 69, 10 * TILE_SIZE, 13 * TILE_SIZE, 64, 0, key_input);
+        mainPlayer = new TankEntity();
+        entityList = new ArrayList<>();
 
-        root.setStyle("-fx-background-color: lightblue;");
-        this.setOnKeyPressed(key_input);
-        this.setOnKeyReleased(key_input);
-        this.setOnMousePressed(mouse_handler);
-        this.setOnMouseReleased(mouse_handler);
+        double center_x = this.getWidth() / 2;
+        double center_y = this.getHeight() / 2;
+
+        spawnEntity(mainPlayer);
+
+        this.setOnMouseMoved(event -> {
+            mouse_x = event.getSceneX();
+            mouse_y = event.getSceneY();
+            mainPlayer.lookAt(mouse_x,mouse_y);
+        });
+        this.setOnMouseClicked(event -> {
+            double x = mainPlayer.getX();
+            double y = mainPlayer.getY();
+            double angle = Math.toDegrees(Math.atan2(mouse_y - y, mouse_x - x));
+            Entity entity = new ProjectileEntity(angle,5, mainPlayer.getX(), mainPlayer.getY());
+            entity.setPosition(x + 45 * (Math.cos(Math.toRadians(angle))), y + 45 * (Math.sin(Math.toRadians(angle))));
+            spawnEntity(entity);
+
+        });
+
+        gameLoop.start();
+
     }
 
-    public void start_main_thread(){
-        main_thread = new Thread(this);
-        main_thread.start();
+    public void spawnEntity(Entity entity) {
+        entityList.add(entity);
+        entity.render(root);
+//        entity.getEntityGroup().toBack();
     }
 
-    @Override
-    public void run() {
-        long nanoInterval = 16666667; // 60 fps
-        long lastEntityCheck = 0;
-        long last_system_time = System.nanoTime();
-        long current_system_time;
-        long delta = 0;
+    public void despawnEntity(Entity entity) {
+        root.getChildren().remove(entity.getEntityGroup());
+    }
 
-        while (main_thread != null) {
+    private final AnimationTimer gameLoop = new AnimationTimer() {
+        private long lastUpdate = 0;
 
-            last_system_time = System.nanoTime() - (System.nanoTime() - last_system_time);
-//            repaint();
-            d1.move();
-
-            mouse_handler.mouse_location_on_screen = MouseInfo.getPointerInfo().getLocation();
-
-//            SwingUtilities.convertPointFromScreen(mouse_handler.mouse_location_on_screen,this);
-            updateMousePosition(mouse_handler.mouse_location_on_screen.x,mouse_handler.mouse_location_on_screen.y);
-
-            if (lastEntityCheck < last_system_time) {
-
-                lastEntityCheck = System.nanoTime() + 1000000000;
-
-                second++;
-                // System.out.println("POS TILE: " + d1.getTileXPosition() + " " + d1.getTileYPosition() + " " + PlayerData.offsets[PlayerData.check_pointer]);
-                //System.out.println("lol");
-
+        @Override
+        public void handle(long now) {
+            if (lastUpdate == 0) {
+                lastUpdate = now;
+                return;
             }
 
-            current_system_time = System.nanoTime();
-            delta = Math.max(nanoInterval - current_system_time + last_system_time, 0);
-            last_system_time = current_system_time;
+            mainPlayer.getEntityGroup().setLayoutX(root.getScene().getWidth() / 2);
+            mainPlayer.getEntityGroup().setLayoutY(root.getScene().getHeight() / 2);
 
-            try {
-                Thread.sleep(delta / 1000000, (int)(delta % 1000000));
-            } catch (InterruptedException e) {
-                break;
+            Iterator it = entityList.iterator();
+            while (it.hasNext()) {
+                Entity e = (Entity) it.next();
+                if(e instanceof ProjectileEntity) {
+                    ProjectileEntity projectile = (ProjectileEntity) e;
+                    if(projectile.distanceFromOriginalPosition() >= 2000){
+                        despawnEntity(projectile);
+                        it.remove();
+                        continue;
+                    }
+                    double angleRadians = Math.toRadians(projectile.getAngle());
+                    double deltaX = projectile.getSpeed() * Math.cos(angleRadians);
+                    double deltaY = projectile.getSpeed() * Math.sin(angleRadians);
+                    projectile.setPosition(projectile.getX() + deltaX, projectile.getY() + deltaY);
+                    System.out.println("Updating projectile");
+                }
             }
+
+            double deltaTime = (now - lastUpdate) / 1_000_000_000.0; // Convert nanoseconds to seconds
+            lastUpdate = now;
         }
-    }
+    };
 
-    public void updateMousePosition(int x, int y) {
-//        mouse_handler.mouse_x = x - (d1.x - Utils.camera.x);
-//        mouse_handler.mouse_y = y - (d1.y - Utils.camera.y);
-    }
-
-    void handleInteractions(Player d1, Graphics g) {
-
-    }
-
-//    @Override
-    public void paintComponent(Graphics g){
-
-//        super.paintComponent(g);
-//
-//        map.view(d1, Utils.camera);
-//        map.displayTiles(g, Utils.camera);
-//        map.verifyEntityPosition(d1);
-//        d1.display(g, Utils.camera);
-
-        handleInteractions(d1, g);
-
-        //debug check coords for spawning purposes
-        // System.out.println("POS: " + d1.x + " " + d1.y);
-        //System.out.println();
-        //System.out.println("POS TILE: " + d1.getTileXPosition() + " " + d1.getTileYPosition());
-    }
 }
--- src/main/java/com/example/bbc/IOGame.java
+++ src/main/java/com/example/bbc/IOGame.java
@@ -19,29 +19,14 @@ import java.util.*;
 public class IOGame extends Application {
 
     static Stage MAIN_STAGE;
-    static Group root;
-    private Entity mainPlayer;
-    private final Set<KeyCode> activeKeys = new HashSet<>();
-    private double mouse_x, mouse_y;
-    public static List<Entity> entityList;
 
     @Override
     public void start(Stage stage) throws IOException {
 
         MAIN_STAGE = stage;
 
-        root = new Group();
-
-        Image backgroundImage = new Image("file:src/main/java/assets/background.png");
-        ImageView backgroundView = new ImageView(backgroundImage);
-        backgroundView.fitHeightProperty().bind(stage.heightProperty());
-        backgroundView.fitWidthProperty().bind(stage.widthProperty());
-
-        root.getChildren().add(backgroundView);
-
-        Scene scene = new Scene(root, 500, 500);
         MAIN_STAGE.setTitle("bbc.io");
-        MAIN_STAGE.setScene(scene);
+        MAIN_STAGE.setScene(Scenes.GAME_SCENE);
         // Maximize window by default
         MAIN_STAGE.setMaximized(true);
 
@@ -49,79 +34,7 @@ public class IOGame extends Application {
         MAIN_STAGE.setFullScreen(false);
         MAIN_STAGE.show();
 
-        mainPlayer = new TankEntity();
-        entityList = new ArrayList<>();
-
-        double center_x = scene.getWidth() / 2;
-        double center_y = scene.getHeight() / 2;
-
-        spawnEntity(mainPlayer);
-
-        scene.setOnMouseMoved(event -> {
-            mouse_x = event.getSceneX();
-            mouse_y = event.getSceneY();
-            mainPlayer.lookAt(mouse_x,mouse_y);
-        });
-        scene.setOnMouseClicked(event -> {
-            double x = mainPlayer.getX();
-            double y = mainPlayer.getY();
-            double angle = Math.toDegrees(Math.atan2(mouse_y - y, mouse_x - x));
-            Entity entity = new ProjectileEntity(angle,5, mainPlayer.getX(), mainPlayer.getY());
-            entity.setPosition(x + 45 * (Math.cos(Math.toRadians(angle))), y + 45 * (Math.sin(Math.toRadians(angle))));
-            spawnEntity(entity);
-
-        });
-
-        gameLoop.start();
     }
-
-    public void spawnEntity(Entity entity) {
-        entityList.add(entity);
-        entity.render(root);
-//        entity.getEntityGroup().toBack();
-    }
-
-    public void despawnEntity(Entity entity) {
-        root.getChildren().remove(entity.getEntityGroup());
-    }
-
-    private final AnimationTimer gameLoop = new AnimationTimer() {
-        private long lastUpdate = 0;
-
-        @Override
-        public void handle(long now) {
-            if (lastUpdate == 0) {
-                lastUpdate = now;
-                return;
-            }
-
-            mainPlayer.getEntityGroup().setLayoutX(root.getScene().getWidth() / 2);
-            mainPlayer.getEntityGroup().setLayoutY(root.getScene().getHeight() / 2);
-
-            Iterator it = entityList.iterator();
-            while (it.hasNext()) {
-                Entity e = (Entity) it.next();
-                if(e instanceof ProjectileEntity) {
-                    ProjectileEntity projectile = (ProjectileEntity) e;
-                    if(projectile.distanceFromOriginalPosition() >= 2000){
-                        despawnEntity(projectile);
-                        it.remove();
-                        continue;
-                    }
-                    double angleRadians = Math.toRadians(projectile.getAngle());
-                    double deltaX = projectile.getSpeed() * Math.cos(angleRadians);
-                    double deltaY = projectile.getSpeed() * Math.sin(angleRadians);
-                    projectile.setPosition(projectile.getX() + deltaX, projectile.getY() + deltaY);
-                    System.out.println("Updating projectile");
-                }
-            }
-
-
-
-            double deltaTime = (now - lastUpdate) / 1_000_000_000.0; // Convert nanoseconds to seconds
-            lastUpdate = now;
-        }
-    };
 
     public static void main(String[] args) {
         launch();
