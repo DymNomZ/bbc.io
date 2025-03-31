712e5578b654c096ce81eb6f77e94baa281c3365
unknown <raymondgtio@gmail.com>
2025-04-01 04:30:30 +0800

    updated naming conventions for upgrade buttons

--- src/main/java/com/example/bbc/GameUIController.java
+++ src/main/java/com/example/bbc/GameUIController.java
@@ -11,13 +11,13 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 public class GameUIController {
-    public Button manualAddXP;  //for visuals only, to be deleted
-    public Label TEMPlblHealth, TEMPlblSpeed, TEMPlblDamage, TEMPlblXP;  //for visuals only, to be deleted
+    public Button TEMP_manual_add_xp;  //for visuals only, to be deleted
+    public Label TEMP_lbl_health, TEMP_lbl_speed, TEMP_lbl_damage, TEMP_lbl_xp;  //for visuals only, to be deleted
     IntegerProperty xp = new SimpleIntegerProperty(0);      //using IntegerProperty allows labels to react to changes to the integer
     IntegerProperty health = new SimpleIntegerProperty(100);
     IntegerProperty speed = new SimpleIntegerProperty(5);
     IntegerProperty damage = new SimpleIntegerProperty(10);
-    BooleanProperty upgradeButtonsEnabled = new SimpleBooleanProperty(false);   //initial state for upgrade buttons
+    BooleanProperty upgrade_buttons_enabled = new SimpleBooleanProperty(false);   //initial state for upgrade buttons
 
     public Button btnUpgradeBulletDamage, btnUpgradeSpeed, btnUpgradeHealth;
     @FXML private ProgressBar healthBar;
@@ -32,16 +32,16 @@ public class GameUIController {
 
     
     public void initialize() {
-        TEMPlblHealth.textProperty().bind(health.asString());   //binds to all changes to health variable
-        TEMPlblSpeed.textProperty().bind(speed.asString());
-        TEMPlblDamage.textProperty().bind(damage.asString());
-        TEMPlblXP.textProperty().bind(xp.asString());
+        TEMP_lbl_health.textProperty().bind(health.asString());   //binds to all changes to health variable
+        TEMP_lbl_speed.textProperty().bind(speed.asString());
+        TEMP_lbl_damage.textProperty().bind(damage.asString());
+        TEMP_lbl_xp.textProperty().bind(xp.asString());
 
-        upgradeButtonsEnabled.set(xp.get() >= 10);  //sets condition for when buttons are enabled or disabled
+        upgrade_buttons_enabled.set(xp.get() >= 10);  //sets condition for when buttons are enabled or disabled
 
-        btnUpgradeHealth.disableProperty().bind(upgradeButtonsEnabled.not());   //binds to changes to upgradeButtonsEnabled variable
-        btnUpgradeSpeed.disableProperty().bind(upgradeButtonsEnabled.not());
-        btnUpgradeBulletDamage.disableProperty().bind(upgradeButtonsEnabled.not());
+        btnUpgradeHealth.disableProperty().bind(upgrade_buttons_enabled.not());   //binds to changes to upgradeButtonsEnabled variable
+        btnUpgradeSpeed.disableProperty().bind(upgrade_buttons_enabled.not());
+        btnUpgradeBulletDamage.disableProperty().bind(upgrade_buttons_enabled.not());
     }
     
     public void toggleDebugInfo() {
@@ -51,23 +51,23 @@ public class GameUIController {
     public void onUpgradeHealth(ActionEvent actionEvent) {
         health.set(health.get() + 5);
         xp.set(xp.get() - 10);
-        upgradeButtonsEnabled.set(xp.get() >= 10);
+        upgrade_buttons_enabled.set(xp.get() >= 10);
     }
 
     public void onUpgradeSpeed(ActionEvent actionEvent) {
         speed.set(speed.get() + 3);
         xp.set(xp.get() - 10);
-        upgradeButtonsEnabled.set(xp.get() >= 10);
+        upgrade_buttons_enabled.set(xp.get() >= 10);
     }
 
     public void onUpgradeDamage(ActionEvent actionEvent) {
         damage.set(damage.get() + 5);
         xp.set(xp.get() - 10);
-        upgradeButtonsEnabled.set(xp.get() >= 10);
+        upgrade_buttons_enabled.set(xp.get() >= 10);
     }
 
-    public void manualAddXP(ActionEvent actionEvent) {
+    public void TEMP_manualAddXP(ActionEvent actionEvent) {
         xp.set(xp.get() + 5);
-        upgradeButtonsEnabled.set(xp.get() >= 10);
+        upgrade_buttons_enabled.set(xp.get() >= 10);
     }
 }
