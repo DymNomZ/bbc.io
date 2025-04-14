689183e40e0a2f4cdbb0d9b792c3269e712f8530
DymNomZ <dymierborgonia@gmail.com>
2025-04-14 17:33:43 +0800

    Added game title, play and about devs button images. Game title and username text field not properly centered.
    Changes:
    - Sprites.java
     - Added images for title, play button and about devs button.
     - TitleScreenController.java
      - Set graphics and appropriate dimensions for each respective element.

--- src/main/java/classes/Sprites.java
+++ src/main/java/classes/Sprites.java
@@ -5,16 +5,44 @@ import javafx.scene.image.ImageView;
 
 public class Sprites {
 
-    public static class Buttons {
+    public static class Titles {
 
-        static final Image PLAY_BUTTON_SPRITE;
+        static final Image TITLE_SPRITE;
 
-        public static final ImageView PLAY_BUTTON;
+        public static final ImageView TITLE;
 
         static {
 
-            PLAY_BUTTON_SPRITE = new Image("file:src/main/java/assets/buttons/temp_btn.jpg");
+            TITLE_SPRITE = new Image("file:src/main/java/assets/titles/title.png");
+            TITLE = new ImageView(TITLE_SPRITE);
+
+        }
+
+    }
+
+    public static class Buttons {
+
+        static final Image PLAY_BUTTON_SPRITE;
+        static final Image ABOUT_DEVS_BUTTON_SPRITE;
+
+        public static final ImageView PLAY_BUTTON;
+        public static final ImageView ABOUT_DEVS;
+
+        static {
+
+            PLAY_BUTTON_SPRITE = new Image("file:src/main/java/assets/buttons/play_button.png");
             PLAY_BUTTON = new ImageView(PLAY_BUTTON_SPRITE);
+            PLAY_BUTTON.setFitWidth(200);
+            PLAY_BUTTON.setFitHeight(200);
+            PLAY_BUTTON.setPreserveRatio(true);
+
+
+            ABOUT_DEVS_BUTTON_SPRITE = new Image("file:src/main/java/assets/buttons/about_devs_button.png");
+            ABOUT_DEVS = new ImageView(ABOUT_DEVS_BUTTON_SPRITE);
+            ABOUT_DEVS.setFitWidth(100);
+            ABOUT_DEVS.setFitHeight(100);
+            ABOUT_DEVS.setPreserveRatio(true);
+
         }
 
     }
--- src/main/java/com/example/bbc/TitleScreenController.java
+++ src/main/java/com/example/bbc/TitleScreenController.java
@@ -9,9 +9,30 @@ public class TitleScreenController {
     public ImageView titleImageView;
     public TextField playerNameTF;
     public Button playBtn;
+    public Button aboutDevsBtn;
 
     public void initialize(){
 
+        String style = "-fx-background-color: transparent;" +
+                "-fx-padding: 0;" +
+                "-fx-background-radius: 0;";
+
         playBtn.setGraphic(Sprites.Buttons.PLAY_BUTTON);
+        playBtn.setFocusTraversable(false);
+        playBtn.setStyle(style);
+
+        aboutDevsBtn.setGraphic(Sprites.Buttons.ABOUT_DEVS);
+        aboutDevsBtn.setFocusTraversable(false);
+        aboutDevsBtn.setStyle(style);
+
+        titleImageView.setImage(Sprites.Titles.TITLE.getImage());
+        titleImageView.setFitWidth(500);
+        titleImageView.setFitHeight(300);
+        titleImageView.setPreserveRatio(true);
+
+    }
+
+    public void test(){
+        System.out.println("lol");
     }
 }
