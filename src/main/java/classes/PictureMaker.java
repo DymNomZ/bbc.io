2800c94fdbd42b1d675cc0ac4e7c1d70d842b808
BladeLucas27 <raymondgtio@gmail.com>
2025-04-21 17:02:57 +0800

    Trying to convert SpriteMaker (allows resizing animated sprites) into more general PictureMaker (ideally allows resizing all kinds of pictures).

---
+++ src/main/java/classes/PictureMaker.java
@@ -0,0 +1,65 @@
+package classes;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PictureMaker {
+    ArrayList<Image> frames;
+    ImageView ivSpriteHolder;
+    VBox vContainer;
+    public PictureMaker(ImageView ivSpriteHolder, VBox vContainer, String name, AnchorPane apDevScreen, boolean isSprite, double divider){
+        this.ivSpriteHolder = ivSpriteHolder;
+        this.vContainer = vContainer;
+        if(isSprite){
+            this.frames = new ArrayList<>();
+            this.frames.add(new Image("file:src/main/java/assets/" + name + "_left_F0-resized.png"));
+            this.frames.add(new Image("file:src/main/java/assets/" + name + "_left_F1-resized.png"));
+            this.frames.add(new Image("file:src/main/java/assets/" + name + "_left_F0-resized.png"));
+            this.frames.add(new Image("file:src/main/java/assets/" + name + "_left_F-1-resized.png"));
+            ivSpriteHolder.setImage(frames.get(0));
+            AtomicInteger currentFrameIndex = new AtomicInteger();
+            Timeline timeline;
+            Duration frameDuration = Duration.millis(400);
+            KeyFrame keyFrame = new KeyFrame(frameDuration, event -> {
+                ivSpriteHolder.setImage(frames.get(currentFrameIndex.get()));
+                currentFrameIndex.set((currentFrameIndex.get() + 1) % 4);
+            });
+            timeline = new Timeline(keyFrame);
+            timeline.setCycleCount(Timeline.INDEFINITE);
+            timeline.play();
+        } else{
+            Image static_img = new Image("file:src/main/java/assets/" + name + ".png");
+            ivSpriteHolder.setImage(static_img);
+        }
+        ivSpriteHolder.setSmooth(false);
+        ivSpriteHolder.setPreserveRatio(true);
+        ivSpriteHolder.fitHeightProperty().bind(vContainer.heightProperty());
+        ivSpriteHolder.fitWidthProperty().bind(vContainer.widthProperty());
+        apDevScreen.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
+                System.out.println("Width: " + newSceneWidth);
+
+                if(newSceneWidth.doubleValue()/1264.0 <= 1.52){
+                    System.out.println("Width divider: " + newSceneWidth.doubleValue()/divider);
+                    ivSpriteHolder.fitHeightProperty().bind(vContainer.heightProperty().divide(newSceneWidth.doubleValue()/divider));
+                    ivSpriteHolder.fitWidthProperty().bind(vContainer.widthProperty().divide(newSceneWidth.doubleValue()/divider));
+                }
+            }
+        });
+        apDevScreen.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
+                System.out.println("Height: " + newSceneHeight);
+            }
+        });
+    }
+}
--- src/main/java/com/example/bbc/DevPageController.java
+++ src/main/java/com/example/bbc/DevPageController.java
@@ -1,20 +1,21 @@
 package com.example.bbc;
 
-import classes.SpriteMaker;
+import classes.PictureMaker;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
 public class DevPageController {
-    public ImageView ivDymSprite, ivSethSprite, ivLanceSprite, ivZillionSprite, ivRaymondSprite;
+    public ImageView ivDymSprite, ivSethSprite, ivLanceSprite, ivZillionSprite, ivRaymondSprite, ivDevTitle;
     public AnchorPane apDevScreen;
-    public VBox vDym, vSeth, vLance, vZillion, vRaymond;
+    public VBox vDym, vSeth, vLance, vZillion, vRaymond, vDevTitle;
 
     public void initialize(){
-        new SpriteMaker(ivDymSprite, vDym, "dymier", apDevScreen);
-        new SpriteMaker(ivSethSprite, vSeth, "seth", apDevScreen);
-        new SpriteMaker(ivLanceSprite, vLance, "lance", apDevScreen);
-        new SpriteMaker(ivZillionSprite, vZillion, "zillion", apDevScreen);
-        new SpriteMaker(ivRaymondSprite, vRaymond, "raymond", apDevScreen);
+        new PictureMaker(ivDymSprite, vDym, "dymier", apDevScreen, true, 1264);
+        new PictureMaker(ivSethSprite, vSeth, "seth", apDevScreen, true, 1264);
+        new PictureMaker(ivLanceSprite, vLance, "lance", apDevScreen, true, 1264);
+        new PictureMaker(ivZillionSprite, vZillion, "zillion", apDevScreen, true, 1264);
+        new PictureMaker(ivRaymondSprite, vRaymond, "raymond", apDevScreen, true, 1264);
+        new PictureMaker(ivDevTitle, vDevTitle, "titles/dev_title", apDevScreen, false, 1328);
     }
 }
