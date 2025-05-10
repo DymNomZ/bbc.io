13b948ac231c7fdf0d294ee3d91d1e1af6dd52b8
DymNomZ <dymierborgonia@gmail.com>
2025-05-10 23:27:08 +0800

    Changed HOSTNAME to eloquenceprojects

--- src/main/java/configs/SocketConfig.java
+++ src/main/java/configs/SocketConfig.java
@@ -1,7 +1,8 @@
 package configs;
 
 public class SocketConfig {
-    public static final String HOSTNAME = "localhost"; //"bbc.eloquenceprojects.org";
+//    public static final String HOSTNAME = "localhost";
+    public static final String HOSTNAME = "bbc.eloquenceprojects.org";
     public static final int PORT = 30006;
     public static byte[] KEY = {98, 98, 99, 95, 118, 101, 114, 121, 95, 115, 101, 99, 117, 114, 101, 100, 95, 107, 101, 121, 95, 110, 105, 103, 103, 97};
 }
