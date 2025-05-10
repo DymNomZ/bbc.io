314e9658621ad604aa7c2c0eb0b30b8114b2780d
Lance <lancejoseph.enario@gmail.com>
2025-05-10 13:10:17 +0800

    Removed debugging area to free up space for chat & death logs & Settings-UI adjustments
    Details:
    - removed/commented code in GameUIController relating to previous temp labels
    - created settings-styles.css

--- src/main/java/com/example/bbc/GameUIController.java
+++ src/main/java/com/example/bbc/GameUIController.java
@@ -21,8 +21,6 @@ import java.io.IOException;
 import static utils.Scenes.GAME_SCENE;
 
 public class GameUIController {
-    public Button TEMP_manual_add_xp;  //for visuals only, to be deleted
-    public Label TEMP_lbl_health, TEMP_lbl_speed, TEMP_lbl_damage, TEMP_lbl_xp;  //for visuals only, to be deleted
     public AnchorPane root;
     public Button TEMPbtnDevPage;
     public Button TEMPbtnLobbyPage;
@@ -35,13 +33,8 @@ public class GameUIController {
 
     public Button btnUpgradeBulletDamage, btnUpgradeSpeed, btnUpgradeHealth;
     @FXML private ProgressBar healthBar;
-    @FXML private Label scoreLabel;
-    @FXML private Label playerNameLabel;
-    @FXML private Pane minimapPane;
     @FXML private VBox debugPanel;
-    @FXML private Label coordsLabel;
-    @FXML private Label fpsLabel;
-    @FXML private Label entityCountLabel;
+
 
     boolean speed_is_upgraded = false;
     boolean health_is_upgraded = false;
@@ -49,11 +42,6 @@ public class GameUIController {
 
     
     public void initialize() {
-        TEMP_lbl_health.textProperty().bind(health.asString());   //binds to all changes to health variable
-        TEMP_lbl_speed.textProperty().bind(speed.asString());
-        TEMP_lbl_damage.textProperty().bind(damage.asString());
-        TEMP_lbl_xp.textProperty().bind(xp.asString());
-
         upgrade_buttons_enabled.set(xp.get() >= 10);  //sets condition for when buttons are enabled or disabled
 
         btnUpgradeHealth.disableProperty().bind(upgrade_buttons_enabled.not());   //binds to changes to upgradeButtonsEnabled variable
@@ -179,23 +167,23 @@ public class GameUIController {
         }
     }
 
-    public void TEMP_devPage(ActionEvent actionEvent) throws IOException {
-//        IOGame.MAIN_STAGE.setScene(Scenes.DEVS_SCENE);
-        FXMLLoader fxmlLoader = new FXMLLoader(IOGame.class.getResource("dev-page.fxml"));
-        Stage stage = (Stage) TEMPbtnDevPage.getScene().getWindow();
-        stage.setScene(new Scene(fxmlLoader.load(), 1280, 720));
-        stage.setTitle("Dev Screen");
-        stage.setScene(stage.getScene());
-        stage.show();
-    }
-
-    public void TEMP_lobbyPage(ActionEvent actionEvent) throws IOException {
-//        IOGame.MAIN_STAGE.setScene(Scenes.DEVS_SCENE);
-        FXMLLoader fxmlLoader = new FXMLLoader(IOGame.class.getResource("game-lobby-ui.fxml"));
-        Stage stage = (Stage) TEMPbtnLobbyPage.getScene().getWindow();
-        stage.setScene(new Scene(fxmlLoader.load(), 1280, 720));
-        stage.setTitle("Lobby Screen");
-        stage.setScene(stage.getScene());
-        stage.show();
-    }
+//    public void TEMP_devPage(ActionEvent actionEvent) throws IOException {
+////        IOGame.MAIN_STAGE.setScene(Scenes.DEVS_SCENE);
+//        FXMLLoader fxmlLoader = new FXMLLoader(IOGame.class.getResource("dev-page.fxml"));
+//        Stage stage = (Stage) TEMPbtnDevPage.getScene().getWindow();
+//        stage.setScene(new Scene(fxmlLoader.load(), 1280, 720));
+//        stage.setTitle("Dev Screen");
+//        stage.setScene(stage.getScene());
+//        stage.show();
+//    }
+//
+//    public void TEMP_lobbyPage(ActionEvent actionEvent) throws IOException {
+////        IOGame.MAIN_STAGE.setScene(Scenes.DEVS_SCENE);
+//        FXMLLoader fxmlLoader = new FXMLLoader(IOGame.class.getResource("game-lobby-ui.fxml"));
+//        Stage stage = (Stage) TEMPbtnLobbyPage.getScene().getWindow();
+//        stage.setScene(new Scene(fxmlLoader.load(), 1280, 720));
+//        stage.setTitle("Lobby Screen");
+//        stage.setScene(stage.getScene());
+//        stage.show();
+//    }
 }
